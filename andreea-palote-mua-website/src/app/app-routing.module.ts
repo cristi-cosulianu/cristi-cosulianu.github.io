472c6c32ebabf36eeabf8956249e987b083bd249
Cristi-Mihail Cosulianu <cristi_cosulianu@yahoo.com>
2023-12-02 22:58:15 +0200

    Logo

--- andreea-palote-mua-website/src/app/app-routing.module.ts
+++ andreea-palote-mua-website/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MaintenanceComponent } from './maintenance/maintenance.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'maintenance', component: MaintenanceComponent},
+  { path: '', redirectTo: '/maintenance', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
